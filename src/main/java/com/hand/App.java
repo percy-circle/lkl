e7248a20e8c3668f8bbae5bec6c949012c2fd66f
蔺凯龙 <563116570@qq.com>
2016-12-29 13:57:18 +0800

    lkl

---
+++ src/main/java/com/hand/App.java
@@ -0,0 +1,11 @@
+package com.hand;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
---
+++ src/main/java/com/hand/User.java
@@ -0,0 +1,16 @@
+package com.hand;
+
+/**
+ * Created by LinKaiLong on 2016/12/29.
+ */
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
